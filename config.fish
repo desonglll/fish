226c401ecc0c2532f2b1abbac4c8e0379cb6022d
desonglll <lindesong666@163.com>
2023-01-08 22:55:06 +0800

    update

--- config.fish
+++ config.fish
@@ -5,5 +5,8 @@ end
 
 alias vi="nvim"
 alias v="nvim"
-alias codes="cd ~/Documents/Github/codes"
-
+alias openvsc="open -a 'Visual Studio Code' "
+alias de="bash deploy.sh"
+alias cl="clear"
+alias t="tree"
+alias web="bash /Users/mikeshinoda/Desktop/vuepress-starter/deploy.sh"
